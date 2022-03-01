524db857826b609e2b8f78e7e973802cd3bd0d62
Chubbs91 <chaskin1991@outlook.com>
2022-02-28 18:17:27 -0600

    setup .env in config

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('homework_db', 'username', 'password', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
