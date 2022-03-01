7edca96b0cd33a022afd95ca5a2c9e8d097c3ca1
Chubbs91 <chaskin1991@outlook.com>
2022-02-28 20:42:39 -0600

    work started on index model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Homework = require('./Homework');
 
+// Create Associations
+User.hasMany(Homework, {
+    foreignKey: 'user_id'
+});
+
+Homework.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Homework };
