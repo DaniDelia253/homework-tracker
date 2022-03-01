d01af25c2415bde707d305a3a7c0d25da93baffc
Chubbs91 <chaskin1991@outlook.com>
2022-02-28 19:28:40 -0600

    user-routes started

---
+++ controller/api/user-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
