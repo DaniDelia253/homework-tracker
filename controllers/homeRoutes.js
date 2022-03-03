20d5f863cd06d10f16d2eb323564353d6d6dbbca
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-02 20:42:50 -0600

    login route added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,4 +32,12 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login')
+})
+
 module.exports = router;
