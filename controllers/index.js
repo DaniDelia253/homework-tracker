915b495e70f36fe15e3fc3825f1820733559f7fd
Chubbs91 <chaskin1991@outlook.com>
2022-03-02 20:30:42 -0600

    resolve merge conflict

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/index');
-<<<<<<< HEAD
 const homeRoutes = require('./homeRoutes');
-=======
->>>>>>> feature/routes
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
