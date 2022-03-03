46956e4fcdb5eec07558fef0ebae7116eb5d46b9
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-02 19:38:44 -0600

    homeroute / route updated

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,23 @@
 const router = require('express').Router();
+const { User, Homework } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+
+    User.findAll({
+        attributes: [
+            "id",
+            "username",
+            "email",
+            "password"
+        ],
+        include: [
+            {
+                model: Homework,
+                attributes: ["id", "title", "homework_text", "user_id"]
+            }
+        ]
+    }).then((dbUserData) => {
+        console.log(dbUserData)
+    }).catch(console.log(err))
+})
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/index');
-const homeRoutes = require('./homeRoutes')
 
-router.use('/', homeRoutes)
 router.use('/api', apiRoutes);
 
 module.exports = router;
