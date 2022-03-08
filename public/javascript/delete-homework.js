11fa6efa1f4f1e6363b40d4a936e7c28aa978b53
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-03-08 15:09:51 -0600

    changes to homework

--- public/javascript/delete-homework.js
+++ public/javascript/delete-homework.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function deleteFormHandler(event, homeworkId) {
   event.preventDefault();
 
@@ -11,23 +10,6 @@ async function deleteFormHandler(event, homeworkId) {
     document.location.replace('/');
   } else {
     alert(response.statusText);
-=======
-async function deleteFormHandler(event) {
-    event.preventDefault();
-  
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/homework/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
->>>>>>> dev/zane
   }
 }
 
