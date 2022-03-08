6a9a6712c98fba2f947818bc717e85419c38f6b3
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-07 19:57:55 -0600

    complete button deletes homework

--- public/javascript/delete-homework.js
+++ public/javascript/delete-homework.js
@@ -1,19 +1,26 @@
-async function deleteFormHandler(event) {
-    event.preventDefault();
-  
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
+async function deleteFormHandler(event, homeworkId) {
+  event.preventDefault();
+
+
+  const response = await fetch(`/api/homework/${homeworkId}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.delete-homework-btn').addEventListener('click', deleteFormHandler);
-  
+}
+
+// document.querySelector('.complete-homework-btn').addEventListener('click', deleteFormHandler);
+
+
+document.querySelectorAll('.complete-homework-btn').forEach(item => {
+  item.addEventListener('click', (event) => {
+    console.log(event)
+    homeworkId = event.target.dataset.id
+    console.log(homeworkId)
+    deleteFormHandler(event, homeworkId)
+  });
+})
