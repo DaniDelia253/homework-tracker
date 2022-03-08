93d4fbe95b23d49b15f3e572e0375762395bd807
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-03-08 17:57:16 -0600

    fixed edit due date picker

--- public/javascript/datepicker.js
+++ public/javascript/datepicker.js
@@ -5,5 +5,12 @@ const picker = datepicker('#dueDate', {
     }
 })
 
-document.querySelector('#dueDate').addEventListener('click', picker);
+const pickerTwo = datepicker('#editDueDate', {
+    formatter: (input, date, instance) => {
+        const value = date.toLocaleDateString()
+        input.value = value // => '1/1/2099'
+    }
+})
 
+document.querySelector('#editDueDate').addEventListener('click', pickerTwo);
+document.querySelector('#dueDate').addEventListener('click', picker);
