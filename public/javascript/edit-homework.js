8135db59268d181999463e9c02a546f6eedc4cd9
Chubbs91 <chaskin1991@outlook.com>
2022-03-07 20:49:10 -0600

    edit HW working. Need to fix DatePicker on Edit

--- public/javascript/edit-homework.js
+++ public/javascript/edit-homework.js
@@ -1,14 +1,18 @@
+let id = "c"
+
 async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="homework-title"]').value.trim();
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
+    const title = document.querySelector('input[id="editHomeworkTitle"]').value;
+    const homework_text = document.querySelector('input[id="editHomeworkText"]').value;
+    const due_date = document.querySelector('input[id="editDueDate"]').value;
+
+    const response = await fetch(`/api/homework/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        title
+        title,
+        homework_text,
+        due_date
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -16,11 +20,16 @@ async function editFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
   }
-  
-  document.querySelector('.edit-homework-form').addEventListener('submit', editFormHandler);
-  
+
+  document.querySelectorAll('.edit-homework-btn').forEach(item => {
+    item.addEventListener('click', (event) => {
+      id = event.target.dataset.id
+    })
+  });
+
+  document.querySelector('.editHomeworkForm').addEventListener('submit', editFormHandler);
