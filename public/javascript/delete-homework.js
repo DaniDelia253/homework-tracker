1493f47f8cd839b085c90a394745c4b3b272275e
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-03-08 15:13:07 -0600

    deleted preventdefault in deete homework

--- public/javascript/delete-homework.js
+++ public/javascript/delete-homework.js
@@ -1,5 +1,4 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
   
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
