d3b5be2d8b5388ce1802cc43f977fc386d668603
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-03-07 20:59:41 -0600

    added deleting a task

---
+++ public/javascript/delete-task.js
@@ -0,0 +1,29 @@
+
+async function deleteFormHandler(id) {
+  
+    const response = await fetch(`/api/task/${id}`, {
+      method: 'DELETE'
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      document.location.replace('/');
+    }
+}
+
+document.querySelectorAll('.delete-task-btn').forEach(item => {
+    item.addEventListener('click', findId)
+    return;
+});
+
+function findId(event) {
+    if (event.target.dataset.id !== null) {
+        const taskId = event.target.dataset.id
+        console.log(taskId);
+        deleteFormHandler(taskId);
+    }
+}
+
+  
+  
