6f4ce36bac06ad260c328236062721172c41bff3
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-08 18:56:41 -0600

    edit form loads current values

--- public/javascript/edit-homework.js
+++ public/javascript/edit-homework.js
@@ -1,35 +1,45 @@
 let id = "c"
 
+
+
+
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[id="editHomeworkTitle"]').value;
-    const homework_text = document.querySelector('input[id="editHomeworkText"]').value;
-    const due_date = document.querySelector('input[id="editDueDate"]').value;
+  event.preventDefault();
 
-    const response = await fetch(`/api/homework/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        title,
-        homework_text,
-        due_date
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
+  const title = document.querySelector('input[id="editHomeworkTitle"]').value;
+  const homework_text = document.querySelector('input[id="editHomeworkText"]').value;
+  const due_date = document.querySelector('input[id="editDueDate"]').value;
+
+  const response = await fetch(`/api/homework/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      homework_text,
+      due_date
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }
-
-  document.querySelectorAll('.edit-homework-btn').forEach(item => {
-    item.addEventListener('click', (event) => {
-      id = event.target.dataset.id
-    })
   });
 
-  document.querySelector('.editHomeworkForm').addEventListener('submit', editFormHandler);
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelectorAll('.edit-homework-btn').forEach(item => {
+  item.addEventListener('click', (event) => {
+    id = event.target.dataset.id
+    document.querySelector('input[id="editHomeworkTitle"]').value = event.target.dataset.title;
+    document.querySelector('input[id="editHomeworkText"]').value = event.target.dataset.homework_text;
+    document.querySelector('input[id="editDueDate"]').value = event.target.dataset.due_date
+  })
+});
+
+
+
+document.querySelector
+
+document.querySelector('.editHomeworkForm').addEventListener('submit', editFormHandler);
