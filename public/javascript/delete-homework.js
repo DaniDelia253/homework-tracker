2d0005446680fab8c4f198b12c07bc5fd5df7dd9
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-03-08 19:55:30 -0600

    favicon added

--- public/javascript/delete-homework.js
+++ public/javascript/delete-homework.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 async function deleteFormHandler(event, homeworkId) {
 
 
@@ -11,18 +10,6 @@ async function deleteFormHandler(event, homeworkId) {
   } else {
     alert(response.statusText);
   }
-=======
-async function deleteFormHandler(id) {
-    const response = await fetch(`/api/homework/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
->>>>>>> 6e857b140809bdce0ab2b996c2ebdc53155ef392
 }
 
 document.querySelectorAll('.complete-homework-btn').forEach(item => {
