6c99ab358bfbeba133c4a549083b066c5362bcf9
Dani Davis <90367091+DaniDelia253@users.noreply.github.com>
2022-12-15 19:37:08 +0000

    change connection port

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 // import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-require('dotenv').config();
+require("dotenv").config();
 
 let sequelize;
 
 // create connection to our database, pass in your MySQL information for username and password
-if (process.env.PORT) {
-  sequelize = new Sequelize(process.env.PORT);
+if (true) {
+	sequelize = new Sequelize("mysqldbprivateservice:3306");
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306        
-  });
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PW,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306,
+		}
+	);
 }
 
 module.exports = sequelize;
